1de91c96029948cae74b21032304f9174202d4cf
Serega <serv3546@gmail.com>
2017-12-25 18:49:18 -0500

    Initial commit

---
+++ app/src/main/java/com/developer35/serega/weatherwidget/WeatherWidget.java
@@ -0,0 +1,14 @@
+package com.developer35.serega.weatherwidget;
+
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class WeatherWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
